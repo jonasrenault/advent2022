2261660d890cd2cdfbe3cf9c90fff39fef9f3874
Jonas Renault <jonasrenault@gmail.com>
2022-12-05 09:56:12 +0100

    day04 part 1

---
+++ day04/day04.py
@@ -0,0 +1,43 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.1
+#   kernelspec:
+#     display_name: Python 3.10.8 ('advent')
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Day 4
+
+# %% [markdown]
+# ### Part 1
+
+# %%
+# Read puzzle input
+with open("input.txt", "r") as f:
+    puzzle = f.readlines()
+
+# %%
+puzzle = [l.strip() for l in puzzle]
+
+# %%
+def intervals_overlap(line):
+    left, right = line.strip().split(",")
+    l1, l2 = left.split("-")
+    r1, r2 = right.split("-")
+    return int(
+        int(l1) >= int(r1)
+        and int(l2) <= int(r2)
+        or int(l1) <= int(r1)
+        and int(l2) >= int(r2)
+    )
+
+
+# %%
+sum(list(map(intervals_overlap, puzzle)))
