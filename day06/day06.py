c9a8b40a3a2ee5b3b7a3aabdf9717d3a8765fec3
Jonas Renault <jonasrenault@gmail.com>
2022-12-06 08:51:46 +0100

    day06 part 1

---
+++ day06/day06.py
@@ -0,0 +1,37 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.1
+#   kernelspec:
+#     display_name: Python 3.10.8 ('advent')
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Day 5
+
+# %% [markdown]
+# ### Part 1
+
+# %%
+# Read puzzle input
+with open("input.txt", "r") as f:
+    puzzle = f.readlines()
+
+# %%
+from collections import Counter
+
+puzzle = puzzle[0].strip()
+marker = None
+for i in range(4, len(puzzle)):
+    if len(Counter(puzzle[i - 4 : i])) == 4:
+        marker = i
+        break
+print(marker)
+
+# %%
