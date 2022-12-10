14ea280a6352aa47d64b28a09f4732dac512079b
Jonas Renault <jonasrenault@gmail.com>
2022-12-10 09:04:08 +0100

    day10 part 2

--- day10/day10.py
+++ day10/day10.py
@@ -38,4 +38,27 @@ signals = [1]
 parse_signal(puzzle, signals)
 
 # %%
-sum([i * signals[i - 1] for i in range(20, 221, 40)])
+print(sum([i * signals[i - 1] for i in range(20, 221, 40)]))
+
+# %% [markdown]
+# ### Part 2
+
+# %%
+def draw_pixels(signals):
+    rows = []
+    for row in range(6):
+        pixels = []
+        for pixel in range(1, 41):
+            cycle = 40 * row + pixel
+            if signals[cycle - 1] <= pixel <= signals[cycle - 1] + 2:
+                pixels.append("#")
+            else:
+                pixels.append(".")
+        rows.append(pixels)
+    return rows
+
+
+# %%
+rows = draw_pixels(signals)
+for row in rows:
+    print(" ".join(row))
