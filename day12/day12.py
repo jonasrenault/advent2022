415f7222c90d6344070c91f50315f73af3f79675
Jonas Renault <jonasrenault@gmail.com>
2022-12-12 11:58:20 +0100

    day12 part 1

--- day12/day12.py
+++ day12/day12.py
@@ -31,28 +31,79 @@ puzzle = [[ord(c) for c in l.strip()] for l in puzzle]
 puzzle = np.array(puzzle)
 
 # %%
+from typing import Tuple, List
+
 dirs = {(1, 0), (0, 1), (-1, 0), (0, -1)}
 
 
-def path(pos, seen, puzzle, end):
-    if np.all(pos == end):
-        return seen + [tuple(pos)]
-    current = puzzle[tuple(pos)]
-    possible = [pos + x for x in dirs]
-    possible = list(
-        filter(
-            lambda x: tuple(x) not in seen
-            and 0 <= x[0] < puzzle.shape[0]
-            and 0 <= x[1] < puzzle.shape[1]
-            and (puzzle[tuple(x)] < current or puzzle[tuple(x)] <= current + 1),
-            possible,
-        )
-    )
-    if not possible:
-        return list(range(puzzle.size))
+def get_neighbors(
+    pos: Tuple[int, int], puzzle: np.ndarray, visited: np.ndarray
+) -> List[Tuple[int, int]]:
+    """
+    Get the possible neighbors for pos, given the heights matrix and the matrix
+    of already visited positions.
 
-    paths = [path(x, seen + [tuple(pos)], puzzle, end) for x in possible]
-    return min(paths, key=lambda x: len(x))
+    Parameters
+    ----------
+    pos : Tuple[int, int]
+        the position to get neighbors for
+    puzzle : np.ndarray
+        the heights matrix
+    visited : np.ndarray
+        the matrix of already visited positions
+
+    Returns
+    -------
+    List[Tuple[int, int]]
+        the list of possible neighbors
+    """
+    neighbors = []
+    for d in dirs:
+        neighbor = (pos[0] + d[0], pos[1] + d[1])
+        if (
+            0 <= neighbor[0] < puzzle.shape[0]
+            and 0 <= neighbor[1] < puzzle.shape[1]
+            and not visited[neighbor]
+            and (puzzle[neighbor] < puzzle[pos] or puzzle[neighbor] <= puzzle[pos] + 1)
+        ):
+            neighbors.append(neighbor)
+
+    return neighbors
+
+
+# %%
+def dijkstra(puzzle: np.ndarray, start: Tuple[int, int]) -> np.ndarray:
+    """
+    Compute the minimum cost to reach each position in the puzzle from the
+    start node.
+
+    Parameters
+    ----------
+    puzzle : np.ndarray
+        the heights of each position
+    start : Tuple[int, int]
+        the starting indices
+
+    Returns
+    -------
+    np.ndarray
+        the cost matrix
+    """
+    visited = np.zeros(puzzle.shape, dtype=bool)
+    costs = np.full(puzzle.shape, puzzle.size)
+    costs[tuple(start)] = 0
+
+    while not visited.all():
+        pos = tuple(
+            min(np.argwhere(visited == False).tolist(), key=lambda x: costs[tuple(x)])
+        )
+        visited[pos] = True
+        n = get_neighbors(pos, puzzle, visited)
+        for x in n:
+            if costs[pos] + 1 < costs[x]:
+                costs[x] = costs[pos] + 1
+
+    return costs
 
 
 # %%
@@ -61,6 +112,6 @@ end = np.argwhere(puzzle == ord("E"))[0]
 input = puzzle.copy()
 input[tuple(start)] = ord("a")
 input[tuple(end)] = ord("z")
-p = path(start, [], input, end)
 
-# %%
+costs = dijkstra(input, tuple(start))
+print(costs[tuple(end)])
