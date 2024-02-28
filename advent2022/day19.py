c585f9bec5f3eff9b24e2f1bbbe975b92678eb4d
Jonas Renault <jonasrenault@gmail.com>
2024-02-28 15:57:37 +0100

    day19

--- advent2022/day19.py
+++ advent2022/day19.py
@@ -1,5 +1,6 @@
 import re
-from functools import lru_cache
+from collections import deque
+from math import prod
 
 from advent2022.utils.utils import Advent
 
@@ -8,120 +9,149 @@ advent = Advent(19)
 
 def main():
     lines = advent.get_input_lines()
-    blueprints = parse_blueprints(lines)
-    total = 0
-    for bp in blueprints:
-        score = score_blueprint(bp)
-        total += bp[0] * score
-    print(1, total)
+    blueprints = get_blueprints(lines)
+    total = sum(bid * search(b) for bid, *b in blueprints)
+    advent.submit(1, total)
+
+    total = prod(search(b, 32) for _, *b in blueprints[:3])
+    advent.submit(2, total)
 
 
-@lru_cache(maxsize=None)
-def score_blueprint(
-    costs: tuple[int, ...],
-    resources: tuple[int, int, int] = (0, 0, 0),
-    bots: tuple[int, int, int] = (1, 0, 0),
-    t: int = 24,
-) -> int:
-    if t <= 1:
-        return 0
+def best_case_scenario(initial_amount: int, robots: int, t: int) -> int:
+    return initial_amount + robots * (t + 1) + t * (t + 1) // 2
 
-    # Max number of bots we need for each resource going forward
-    need_obs = bots[2] < costs[6]
-    need_clay = bots[1] < costs[4]
-    need_ore = bots[0] < max(costs[1], costs[2], costs[3], costs[5])
 
-    # Which type of bot should we build ?
-    build_geode = resources[0] >= costs[5] and resources[2] >= costs[6]
-    build_obs = resources[0] >= costs[3] and resources[1] >= costs[4] and need_obs
-    build_clay = resources[0] >= costs[2] and need_clay and need_obs
-    build_ore = resources[0] >= costs[1] and need_ore
+def search(blueprint: tuple[int, ...], time=24) -> int:
+    rore_cost, rclay_cost, robs_cost_ore, robs_cost_clay, rgeo_cost_ore, rgeo_cost_obs = (
+        blueprint
+    )
+    max_ore_needed = max(rore_cost, rclay_cost, robs_cost_ore, rgeo_cost_ore)
+    max_clay_needed = robs_cost_clay
+    max_obs_needed = rgeo_cost_obs
 
-    score = 0
-    if build_geode:
-        # If we can build geode bot, this is the best option
-        resources_ = (
-            resources[0] + bots[0] - costs[5],
-            resources[1] + bots[1],
-            resources[2] + bots[2] - costs[6],
-        )
-        score = max(
-            score,
-            score_blueprint(costs, resources=resources_, bots=bots, t=t - 1) + t - 1,
-        )
-    elif build_obs:
-        # If we can build obsidian bot, this is the second best option
-        resources_ = (
-            resources[0] + bots[0] - costs[3],
-            resources[1] + bots[1] - costs[4],
-            resources[2] + bots[2],
-        )
-        score = max(
-            score,
-            score_blueprint(
-                costs,
-                resources=resources_,
-                bots=(bots[0], bots[1], bots[2] + 1),
-                t=t - 1,
-            ),
-        )
-    else:
-        # Otherwise, consider building ore bot, clay bot, or simply mining
-        if build_ore:
-            resources_ = (
-                resources[0] + bots[0] - costs[1],
-                resources[1] + bots[1],
-                resources[2] + bots[2],
+    best = 0
+    visited = set()
+    q = deque([(time, 0, 0, 0, 0, 1, 0, 0, 0)])
+
+    while q:
+        time, ore, clay, obs, geo, rore, rclay, robs, rgeo = state = q.pop()
+        if state in visited:
+            continue
+
+        visited.add(state)
+
+        # Each robot we have mines 1 resource of its type, taking 1 minute.
+        newore = ore + rore
+        newclay = clay + rclay
+        newobs = obs + robs
+        newgeo = geo + rgeo
+        time -= 1
+
+        # If we run out of time, we reached a "goal" state. Update the best
+        # number of geodes we were able to mine.
+        if time == 0:
+            best = max(best, newgeo)
+            continue
+
+        if best_case_scenario(newgeo, rgeo, time) < best:
+            continue
+
+        if (
+            best_case_scenario(newobs, robs, time) < rgeo_cost_obs
+            or best_case_scenario(newore, rore, time) < rgeo_cost_ore
+        ):
+            best = max(best, newgeo + rgeo * time)
+            continue
+
+        if obs >= rgeo_cost_obs and ore >= rgeo_cost_ore:
+            q.append(
+                (
+                    time,
+                    newore - rgeo_cost_ore,
+                    newclay,
+                    newobs - rgeo_cost_obs,
+                    newgeo,
+                    rore,
+                    rclay,
+                    robs,
+                    rgeo + 1,
+                )
             )
-            score = max(
-                score,
-                score_blueprint(
-                    costs,
-                    resources=resources_,
-                    bots=(bots[0] + 1, bots[1], bots[2]),
-                    t=t - 1,
-                ),
-            )
-        if build_clay:
-            resources_ = (
-                resources[0] + bots[0] - costs[2],
-                resources[1] + bots[1],
-                resources[2] + bots[2],
-            )
-            score = max(
-                score,
-                score_blueprint(
-                    costs,
-                    resources=resources_,
-                    bots=(bots[0], bots[1] + 1, bots[2]),
-                    t=t - 1,
-                ),
-            )
-        resources_ = (
-            resources[0] + bots[0],
-            resources[1] + bots[1],
-            resources[2] + bots[2],
-        )
-        score = max(
-            score, score_blueprint(costs, resources=resources_, bots=bots, t=t - 1)
-        )
 
-    return score
+        if robs < max_obs_needed and clay >= robs_cost_clay and ore >= robs_cost_ore:
+            q.append(
+                (
+                    time,
+                    newore - robs_cost_ore,
+                    newclay - robs_cost_clay,
+                    newobs,
+                    newgeo,
+                    rore,
+                    rclay,
+                    robs + 1,
+                    rgeo,
+                )
+            )
+
+        if rclay < max_clay_needed and ore >= rclay_cost:
+            q.append(
+                (
+                    time,
+                    newore - rclay_cost,
+                    newclay,
+                    newobs,
+                    newgeo,
+                    rore,
+                    rclay + 1,
+                    robs,
+                    rgeo,
+                )
+            )
+
+        if rore < max_ore_needed and ore >= rore_cost:
+            q.append(
+                (
+                    time,
+                    newore - rore_cost,
+                    newclay,
+                    newobs,
+                    newgeo,
+                    rore + 1,
+                    rclay,
+                    robs,
+                    rgeo,
+                )
+            )
+
+        if (
+            (robs and obs < max_obs_needed)
+            or (rclay and clay < max_clay_needed)
+            or ore < max_ore_needed
+        ):
+            q.append(
+                (
+                    time,
+                    newore,
+                    newclay,
+                    newobs,
+                    newgeo,
+                    rore,
+                    rclay,
+                    robs,
+                    rgeo,
+                )
+            )
+
+    return best
 
 
-def parse_blueprints(lines: list[str]) -> list[tuple[int, ...]]:
+def get_blueprints(lines: list[str]) -> list[tuple[int, ...]]:
+    exp = re.compile(r"\d+")
     blueprints = []
+
     for line in lines:
-        m = re.match(
-            r"Blueprint (\d+): Each ore robot costs (\d+) ore. "
-            r"Each clay robot costs (\d)+ ore. "
-            r"Each obsidian robot costs (\d+) ore and (\d+) clay. "
-            r"Each geode robot costs (\d+) ore and (\d+) obsidian.",
-            line,
-        )
-        if m is not None:
-            blueprint = tuple(int(x) for x in m.groups())
-            blueprints.append(blueprint)
+        blueprints.append(tuple(map(int, exp.findall(line))))
+
     return blueprints
 
 
